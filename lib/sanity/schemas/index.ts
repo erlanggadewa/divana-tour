1a46f3d399a81d2818089e2cb98181060531c3ae
Erlangga Dewa <erlanggadewa7@gmail.com>
2024-05-20 14:09:01 +0700

    chore: Remove unused files and code
    The commit removes several unused files and code, including `.gitignore` changes, deleted files in the `utils` directory, deleted files in the `app` directory, deleted files in the `components` directory, and deleted files in the `lib/sanity` directory.

--- lib/sanity/schemas/index.ts
+++ lib/sanity/schemas/index.ts
@@ -1,7 +1,13 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import settings from './settings'
+import author from "./author";
+import blockContent from "./blockContent";
+import category from "./category";
+import post from "./post";
+import settings from "./settings";
 
-export const schemaTypes = [post, author, category, settings, blockContent]
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  settings,
+  blockContent
+];
